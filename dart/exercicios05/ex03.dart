e49175e2529935fc9abb1b6da2727ce47e955ce5
Carlos Cardoso <cvcardos5@gmail.com>
2023-07-28 19:55:47 -0300

    ex03

--- dart/exercicios05/ex03.dart
+++ dart/exercicios05/ex03.dart
@@ -1,22 +1,15 @@
 import 'dart:math';
-Future<String> tarefa1() async {
+//tarefa com duração aleatória
+Future<String> tarefa() async {
   final int duration = Random().nextInt(5) + 1;
   final String data = await Future.delayed(Duration(seconds: duration), () => 'Tarefa 1 concluída em $duration segundos');
   return data;
 }
-Future<String> tarefa2() async {
-  final int duration = Random().nextInt(5) + 1;
-  final String data = await Future.delayed(Duration(seconds: duration), () => 'Tarefa 2 concluída em $duration segundos');
-  return data;
-}
-Future<String> tarefa3() async {
-  final int duration = Random().nextInt(5) + 1;
-  final String data = await Future.delayed(Duration(seconds: duration), () => 'Tarefa 3 concluída em $duration segundos');
-  return data;
-}
 
 main() async {
-  final List<Future<String>> listaTarefas = [tarefa1(), tarefa2(), tarefa3()];
+  final List<Future<String>> listaTarefas = [tarefa(), tarefa(), tarefa()];
+
+  //aguarda todas as tarefas serem concluídas
   final futuresTarefas = await Future.wait(listaTarefas).then((tarefas) {
     for (final tarefa in tarefas) {
       print(tarefa);
